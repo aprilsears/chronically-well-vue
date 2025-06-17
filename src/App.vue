627036c232f782585042b1a04660b30d007c2ca3
April Sears <aprilnsears@gmail.com>
2025-06-17 19:25:40 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+  <div id="app">
+    <header>
+      <!-- Your header content will go here -->
+    </header>
+
+    <main>
+      <!-- Your main content will go here -->
+    </main>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
